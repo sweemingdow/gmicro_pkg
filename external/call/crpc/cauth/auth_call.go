04cc3f25f1a97836e1365e74d0905d72ccee85a2
wsdg <wsdg@gmail.com>
2025-12-06 13:45:23 +0800

    f

--- external/call/crpc/cauth/auth_call.go
+++ external/call/crpc/cauth/auth_call.go
@@ -1,15 +1,7 @@
 package cauth
 
 import (
-	"gmicro_pkg/pkg/server/srpc/rclient"
-	"gmicro_pkg/pkg/server/srpc/rclient/rcfactory"
 	"gmicro_pkg/pkg/server/srpc/rpccall"
-	"time"
-)
-
-const (
-	rpcServerName = "auth_service"
-	defaultTimout = 2 * time.Second
 )
 
 type AuthReq struct {
@@ -29,58 +21,17 @@ type UpdateReq struct {
 	RealAuthed int    `json:"realAuthed"`
 }
 
+// @rpc_server two_service
+//
+//go:generate rpcgen -type AuthRpcProvider
 type AuthRpcProvider interface {
+	// @path /auth
+	// @timeout 1s
 	Auth(req rpccall.RpcReqWrapper[AuthReq]) (rpccall.RpcRespWrapper[AuthResp], error)
 
+	// @timeout 2s
 	Clear(req rpccall.RpcReqWrapper[ClearReq]) (rpccall.RpcRespSimple, error)
 
+	// @path /update_token
 	UpdateToken(req rpccall.RpcReqWrapper[UpdateReq]) (rpccall.RpcRespSimple, error)
-}
-
-type authRpcProvider struct {
-	clientFactory rcfactory.ArpcClientFactory
-}
-
-func NewAuthRpcProvider(clientFactory rcfactory.ArpcClientFactory) AuthRpcProvider {
-	return &authRpcProvider{
-		clientFactory: clientFactory,
-	}
-}
-
-func (arp *authRpcProvider) Auth(req rpccall.RpcReqWrapper[AuthReq]) (rpccall.RpcRespWrapper[AuthResp], error) {
-	cp := arp.acquireClientProxy()
-
-	var resp rpccall.RpcRespWrapper[AuthResp]
-	if err := cp.Call("/auth", &req, &resp, defaultTimout); err != nil {
-		return resp, err
-	}
-
-	return resp, nil
-}
-
-func (arp *authRpcProvider) Clear(req rpccall.RpcReqWrapper[ClearReq]) (rpccall.RpcRespSimple, error) {
-	cp := arp.acquireClientProxy()
-
-	var resp rpccall.RpcRespSimple
-	if err := cp.Call("/clear", &req, &resp, defaultTimout); err != nil {
-		return resp, err
-	}
-
-	return resp, nil
-}
-
-func (arp *authRpcProvider) UpdateToken(req rpccall.RpcReqWrapper[UpdateReq]) (rpccall.RpcRespSimple, error) {
-	cp := arp.acquireClientProxy()
-
-	var resp rpccall.RpcRespSimple
-
-	if err := cp.Call("/update_token", &req, &resp, defaultTimout); err != nil {
-		return resp, err
-	}
-
-	return resp, nil
-}
-
-func (arp *authRpcProvider) acquireClientProxy() rclient.ArpcClientProxy {
-	return arp.clientFactory.AcquireClient(rpcServerName)
 }
